f116c08ca57c3460fea098c089ce7bc86bf9bdbb
Jordan Szubert <jordan@szubert.tk>
2021-05-05 15:32:09 +0200

    parse cmdline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use clap::Clap;
+
+#[derive(Clap, Debug)]
+struct Args {
+    input: String,
+    output: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let input_file_name=args.input;
+    let output_file_name=args.output.unwrap_or(format!("{}.png",input_file_name));
+    println!("Hello, world!\n{:?}\n{:?}",input_file_name,output_file_name);
 }
